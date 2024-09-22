40a48db3c9521fdf5896c3a8daf4315da7cac4eb
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-09-22 16:46:17 +0330

    add product fn

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import HandleError from "./Utils/handleError.js";
 import catchError from "./Utils/catchError.js";
 import userRouter from "./Routes/user.route.js"
 import authRouter from "./Routes/auth.route.js"
+import productRouter from "./Routes/product.route.js  "
 
 const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
@@ -19,6 +20,7 @@ app.use(express.static("Public"));
 // Routes
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
+app.use("/api/product", productRouter)
 
 // Error Handling
 app.use("*", (req, res, next) => {
